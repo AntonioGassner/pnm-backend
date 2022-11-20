337bb28f0a7b10c52919ed4b6687c96a6a14e6cf
Antonio Gassner <antoniobimbo.gassner@studenti.unicam.it>
2022-11-20 13:59:22 +0100

    .

--- src/main/java/it/unicam/pnm/PnmApplication.java
+++ src/main/java/it/unicam/pnm/PnmApplication.java
@@ -2,11 +2,10 @@ package it.unicam.pnm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-// TODO add Azienda, azienda/prodotto, Address classes
-
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PnmApplication {
 
     public static void main(String[] args) {

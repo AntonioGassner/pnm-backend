ac3af3b7e160a8b9b2ffaac29ecb81bb4a316cd3
AntonioGassner <antoniobimbo.gassner@studenti.unicam.it>
2023-01-19 14:30:36 +0100

    fixed CORS errors

--- src/main/java/it/unicam/pnm/PnmApplication.java
+++ src/main/java/it/unicam/pnm/PnmApplication.java
@@ -3,11 +3,11 @@ package it.unicam.pnm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// TODO delete is not mapped or something
-//  Resolved [org.springframework.web.HttpRequestMethodNotSupportedException: Request method 'DELETE' not supported]
-
-// TODO search doesen't pass over criteria or something since every field i put in i always get all results
 
 // TODO security layer
 
@@ -21,8 +21,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 // TODO mailing list?
 
-// TODO how to handle delete of db entries
-
 // TODO mail notifications when someone registers and is waiting for approval
 
 // TODO validation for foreign keys when adding entries at Service layer
@@ -30,7 +28,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 //TODO sistemare la faccenda foreign keys con le annotations
 // non sono sicuro che serve perché possiamo sempre cercare le entry che ci interessano usando il relativo UUID
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+)
 public class PnmApplication {
 
     public static void main(String[] args) {
@@ -38,3 +38,4 @@ public class PnmApplication {
     }
 
 }
+

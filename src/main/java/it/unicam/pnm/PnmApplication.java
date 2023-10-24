d0f40cbe5209d108f014db513e17480f6237ee14
AntonioGassner <antoniobimbo.gassner@studenti.unicam.it>
2023-10-24 16:08:09 +0200

    Update PnmApplication.java

--- src/main/java/it/unicam/pnm/PnmApplication.java
+++ src/main/java/it/unicam/pnm/PnmApplication.java
@@ -1,23 +1,8 @@
 package it.unicam.pnm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-// TODO security layer
-
-// TODO session tokens
-
-// TODO logging system
-
-// TODO admin view/flow to publish changes ?
-
-// TODO encripted db?
-
-// TODO mailing list?
-
-// TODO mail notifications when someone registers and is waiting for approval
-
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
@@ -29,4 +14,3 @@ public class PnmApplication {
     }
 
 }
-
